c4a232f9127cdef370c173963693d3f16184b883
David Betancourt <d.a.betancourt@gmail.com>
2016-01-20 15:41:16 -0600

    added to master

---
+++ PQCapstone/WebContent/WEB-INF/views/out.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=ISO-8859-1" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="mvc" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<title>User Table Dump </title>
+</head>
+
+<body>
+<div class="container-fluid">
+
+</div>
+</body>
+</html>
---
+++ PQCapstone/src/edu/quest/java/config/SpringMVCCapstone.java
@@ -0,0 +1,24 @@
+package edu.quest.java.config;
+
+import java.util.Random;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages={"edu.quest.java"})
+public class SpringMVCCapstone {
+	
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+}
