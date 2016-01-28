26b2097a3ad55828040848472c3ac62c66aaa250
Jewels27 <yramz27@yahoo.com>
2016-01-28 16:19:13 -0600

    updated

---
+++ PQCapstone/src/edu/toosquare/java/Product.java
@@ -0,0 +1,40 @@
+package edu.toosquare.java;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private Product product;
+	
+	public Product(){}
+	
+	Product(Product product){
+
+	}
+	public Product(long id, Product product) {
+		this.id = id;
+		this.product = product;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+}
